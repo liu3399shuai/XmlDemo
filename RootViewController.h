561effe0ec36eb58d83ee4eb8831cd874d45972d
liushuai <liu3399shuai@163.com>
2016-12-05 16:10:03 +0800

    测试1

--- RootViewController.h
+++ RootViewController.h
@@ -9,8 +9,6 @@
 #import <UIKit/UIKit.h>
 #import "HttpDownLoad.h"
 #import "EGORefreshTableHeaderView.h"
-// 关于添加文件资源须知(不能托) 
-// 在工程名字上 右击 Add Files to "..."  并且选中 Destination 的按钮 选中文件或文件夹 按添加
 
 @interface RootViewController : UIViewController <UITableViewDelegate,UITableViewDataSource,HttpDownLoadDelegate,EGORefreshTableHeaderDelegate>
 {
